c46042ee64efb63e8aece57435123ca4354c69d7
Kumar Kunal <hqzn6501@orange.com>
2019-06-11 09:22:59 +0530

    button switch added in the commit

--- App.js
+++ App.js
@@ -1,70 +1,72 @@
-import React,{Component} from 'react';
-import {Image,View,Text,StyleSheet,ScrollView} from 'react-native';
+import React, { Component } from 'react';
+import { Image, View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 
 
-const myStyle =  StyleSheet.create({
- myBasicComp:{
-   flex:1,
-   justifyContent:"center",
-   alignItems:"center",
-   backgroundColor:"blue",
- },
- myBlueComp:{
-  flex:1,
-  justifyContent:"center",
-  alignItems:"center",
-  backgroundColor:"white",
-},
-myTextColor:{
-  color:"green"
-}
+const myStyle = StyleSheet.create({
+  myBasicComp: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
+  myBlueComp: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  myTextColor: {
+    color: "green"
+  }
 
 
 });
 
-export default class MyBasicApp extends Component{
+export default class MyBasicApp extends Component {
 
-  render(){
-      return (<View style={myStyle.myBasicComp}>
-        
-        <Text>Hello World</Text>
-      </View>
-      );
+
+  constructor(props) {
+    super(props)
+    this.state = { btnPressedState: "Not Pressed",times:0 }
+
+    setInterval(() => {
+
+      this.setState(prevState => ({ times: prevState.times  }
+     //   { btnPressedState: "String_3" }        
+      ))
+    }, 10000);
+  }
+
+  onMyBtnPressed = () => {
+    this.state.times=this.state.times+1;
+    if (this.state.times % 2 == 0) {
+       this.setState({ btnPressedState: "Button Pressed" })
+    }
+    else {
+      this.setState({ btnPressedState: "Not Pressed" })
+    }
+  }
+
+
+  render() {
+    return (<View style={myStyle.myBasicComp}>
+
+      <Text style={{ width: 50, height: 50 }}>Hello World {this.state.times} </Text>
+      <Button title={this.state.btnPressedState} onPress={this.onMyBtnPressed}/>
+    </View>
+    );
 
   }
 }
 
-export class MyBlueComp extends Component{
+export class MyBlueComp extends Component {
 
-  render(){
+  render() {
     return (<View style={myStyle.myBlueComp}>
       <Text style={myStyle.myTextColor}>Hello World in Gold</Text>
-    
+
     </View>
     );
   }
 
-}
-
-class ScrollingImageWithText extends Component {
-  render() {
-    return (
-      <ScrollView>
-        <Image
-          source={{uri: 'https://facebook.github.io/react-native/img/header_logo.png'}}
-          style={{width: 66, height: 58}}
-        />
-        <Text>
-          On iOS, a React Native ScrollView uses a native UIScrollView.
-          On Android, it uses a native ScrollView.
-
-          On iOS, a React Native Image uses a native UIImageView.
-          On Android, it uses a native ImageView.
-
-          React Native wraps the fundamental native components, giving you
-          the performance of a native app using the APIs of React.
-        </Text>
-      </ScrollView>
-    );
-  }
 }
